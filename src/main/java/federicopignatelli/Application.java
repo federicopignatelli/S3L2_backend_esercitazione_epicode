4918c3ffc183026a23d414f79a47ceeed136fed4
federicopignatelli <federicopignatelli@icloud.com>
2023-12-12 16:43:49 +0100

    creazione progetto maven collegato al database gestioneeventi

---
+++ src/main/java/federicopignatelli/Application.java
@@ -0,0 +1,20 @@
+package federicopignatelli;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestioneeventi");
+
+    public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+
+
+        System.out.println("Hello World!");
+
+
+        em.close();
+        emf.close();
+    }
+}
